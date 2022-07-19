b516f084b38f267c1eb9a9d633e38859228fba94
billal1993 <m.billalyaqoob@gmail.com>
2022-07-19 18:57:21 +0500

    adding dummy style to navbar

--- src/navbar.js
+++ src/navbar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div>
-      <nav>
+      <nav className="nav-rapper">
         <h3>Emaarat.com</h3>
         <Link to="/">Home</Link>
         <Link to="/properties">Properties</Link>
