d90eb5455b486a8878ab6bd2f6df880beb2a2058
billal1993 <m.billalyaqoob@gmail.com>
2022-07-20 20:18:50 +0500

    navbar initial layout

--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ import Properties from "./properties";
 import Projects from "./projects";
 import Blogs from "./blogs";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/properties" element={<Properties />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/blogs" element={<Blogs />} />
-    </Routes>
-  </BrowserRouter>
+  <div className="appWrapper">
+    <Navbar />
+    <div className="appContent">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/properties" element={<Properties />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/blogs" element={<Blogs />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  </div>
 );
--- src/properties.js
+++ src/properties.js
@@ -1,8 +1,6 @@
-import Navbar from "./navbar";
 function Properties() {
   return (
     <div>
-      <Navbar />
       <h1>Properties will list here</h1>
     </div>
   );
